c6875ee881714a1213f999a5f68ed93413c539e4
zotel1 <zotelsigel@gmail.com>
2026-03-03 21:50:47 -0300

    Primer commit

---
+++ src/main/java/com/mundolimpio/MundoLimpioApplication.java
@@ -0,0 +1,13 @@
+package com.mundolimpio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MundoLimpioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MundoLimpioApplication.class, args);
+	}
+
+}
